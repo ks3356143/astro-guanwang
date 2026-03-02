66ad7badabe220720afa0c1fc8bf7394694ce4c7
chenjunyi <314298729@qq.com>
2026-03-02 16:44:09 +0800

    完成教程内容

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,15 @@
+// @ts-check
+
 import { defineConfig } from "astro/config"
 import tailwindcss from "@tailwindcss/vite"
+
+import preact from "@astrojs/preact"
 
 export default defineConfig({
     vite: {
         plugins: [tailwindcss()],
     },
+
+    site: "https://chen-astro-guanwang.netlify.app/",
+    integrations: [preact()],
 })
